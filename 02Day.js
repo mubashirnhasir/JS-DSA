df37683c2920cf8dac3d519312b2d42acabf5a55
mubashirnhasir <mubashirnhasir@gmail.com>
2025-02-28 01:00:12 +0530

    asa

--- 02Day.js
+++ 02Day.js
@@ -61,8 +61,8 @@ class myArray{
     deleteByIndex(index){
         const item = this.data[index]
 
-        for(let i =0; i<this.length;i++){
-            this.data[i] =this.data[i+1]
+        for(let i = 0; i<this.length;i++){
+            this.data[i] = this.data[i+1]
         }
 
         delete this.data[this.length - 1]
