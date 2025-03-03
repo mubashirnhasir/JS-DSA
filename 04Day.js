5b26518ea8a62e43b6d9d7446e914b2a2df5508e
mubashirnhasir <mubashirnhasir@gmail.com>
2025-03-03 16:22:24 +0530

    FizzBuzz Probnlem

--- 04Day.js
+++ 04Day.js
@@ -1,1 +1,17 @@
 //Day 4 of DSA with JS
+// FizzBuzz Problem
+// let n =200
+// for(let i =1; i <=n;i++){
+//     if(i%3 === 0){
+//         console.log("Fuzzz")
+//     }
+//     else if(i%5 === 0 ){
+//         console.log("Buzzz")
+//     }
+//     else if (i%3 === 0 && i%5 === 0){
+//         console.log("FizzBuzz")
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
