35ba494cb0bef58aebe6a0ae9a266ff89dfc18e6
mubashirnhasir <mubashirnhasir@gmail.com>
2025-03-08 00:43:37 +0530

    range question

--- 05Day.js
+++ 05Day.js
@@ -1,18 +1,68 @@
 // Conditionals in JS & Its type of questions
 
-// Range Question give the payable amount for 5,10,20% of discount
+// Range Question give the payable amount for 5,10,20% of discount Shop bill
 
-let amount = 10000;
+// let amount = 10000;
 
-if(amount>0 && amount <=5000){
-    console.log(amount)
-} 
-else if(amount>5000 && amount <= 7000){
-    console.log(amount - Math.floor((5*amount)/100))
+// if(amount>0 && amount <=5000){
+//     console.log(amount)
+// } 
+// else if(amount>5000 && amount <= 7000){
+//     console.log(amount - Math.floor((5*amount)/100))
+// }
+// else if(amount> 7000 && amount<= 9000){
+//     console.log(amount - Math.floor((10*amount)/100))
+// }
+// else if(amount>9000){
+//     console.log(amount - Math.floor((20*amount)/100))
+// }
+
+// let units = 123
+// let amount = 0
+// if(units > 400){
+//    amount = (units-400) *13
+//    units = 400;
+// } 
+// if(units >200 && units<=400){
+//     amount += (units-200)*8
+//     units =200
+// }
+
+// if(units >100 && units<=200){
+//     amount += (units-100)*6
+//     units =100;
+// }
+
+// amount+=units*4
+
+// console.log(amount)
+
+
+
+// INR Denomination
+
+
+let amount = 280
+console.log("For Given Amount ---- ", amount)
+if(amount >=500){
+    console.log("500 Rupees Note",Math.floor(amount/500))
+    amount = amount%500
 }
-else if(amount> 7000 && amount<= 9000){
-    console.log(amount - Math.floor((10*amount)/100))
+if(amount >200 && amount <500 ){
+    console.log("200 Rupees Note :",Math.floor(amount/200))
+    amount = amount%200
 }
-else if(amount>9000){
-    console.log(amount - Math.floor((20*amount)/100))
+if(amount >100 && amount <200 ){
+    console.log("100 Rupees Note :",Math.floor(amount/100))
+    amount = amount%100
 }
+if(amount >50 && amount <100 ){
+    console.log("50 Rupees Note :",Math.floor(amount/50))
+    amount = amount%50
+}
+if(amount >20 && amount <50 ){
+    console.log("20 Rupees Note :",Math.floor(amount/20))
+    amount = amount%20
+}
+
+console.log("Chillar Rupees :",amount)
